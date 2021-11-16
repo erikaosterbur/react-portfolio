3204a8d6936b2af6460b2a62ae0b673d61858660
erikaosterbur <erikaosterbur@gmail.com>
2021-11-16 14:58:48 -0600

    initial file setup

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio of Work</h1>
+      <p>
+        My work
+      </p>
+    </div>
+  );
+}
