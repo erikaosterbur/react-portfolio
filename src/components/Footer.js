fec1c7a41718782ddb5ff02fed0e0252abe9162a
erikaosterbur <erikaosterbur@gmail.com>
2021-11-16 20:28:33 -0600

    hover over icon color change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,36 @@ import { FaGithub, FaTwitterSquare, FaLinkedin } from "react-icons/fa";
 const style = { color: "black", fontSize: "5em", padding: "10px" }
 
 function Footer() {
+    const handleMouseEnter = e => {
+        e.target.style.color = "grey"
+    }
+    const handleMouseLeave = e => {
+        e.target.style.color = "black"
+    }
     return (
         <footer className="footer">
           <div className="container text-center">
-            <a href="https://github.com/erikaosterbur" target="_blank" rel="noreferrer"><FaGithub style={style}/></a>
-            <a href="https://twitter.com/erika_osterbur" target="_blank" rel="noreferrer"><FaTwitterSquare style={style}/></a>
-            <a href="https://www.linkedin.com/in/erika-osterbur" target="_blank" rel="noreferrer"><FaLinkedin style={style}/></a>
+            <a 
+                onMouseEnter={handleMouseEnter} 
+                onMouseLeave={handleMouseLeave} 
+                href="https://github.com/erikaosterbur" 
+                target="_blank" rel="noreferrer">
+                    <FaGithub style={style}/>
+            </a>
+            <a 
+                onMouseEnter={handleMouseEnter} 
+                onMouseLeave={handleMouseLeave} 
+                href="https://twitter.com/erika_osterbur" 
+                target="_blank" rel="noreferrer">
+                    <FaTwitterSquare style={style}/>
+            </a>
+            <a 
+                onMouseEnter={handleMouseEnter} 
+                onMouseLeave={handleMouseLeave} 
+                href="https://www.linkedin.com/in/erika-osterbur" 
+                target="_blank" rel="noreferrer">
+                    <FaLinkedin style={style}/>
+            </a>
           </div>
         </footer>
     )
