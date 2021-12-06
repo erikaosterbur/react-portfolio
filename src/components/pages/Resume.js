c2feaab6cbd8b487cab48029d177c9a8e4d04e79
erikaosterbur <erikaosterbur@gmail.com>
2021-12-06 12:21:22 -0600

    updated resume link

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,7 +6,7 @@ export default function Resume() {
     <div className="m-4">
       <h1 className="d-flex justify-content-center"><span>My Resume</span></h1>
       <a
-        href="https://drive.google.com/file/d/1I2zj777ZyLxeEKUUFizDtAgWahoDtMzt/view?usp=sharing" 
+        href="https://drive.google.com/file/d/1irPWh0fKR2mZgWZDDf83BG1uqgB1wxSs/view?usp=sharing" 
         target="_blank" rel="noreferrer">
           <img id="resume" src={MyResume} alt="resume"/>
       </a>
